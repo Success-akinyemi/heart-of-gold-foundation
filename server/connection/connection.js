0fd2d7e2f1264ed7a165d9d73b6aee296679bdf3
Success-akinyemi <brownsamuel915@gmaial.com>
2023-10-19 09:37:54 +0100

    server setup

--- server/connection/connection.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-import { config } from 'dotenv';
-config();
-
-mongoose.connect(`${process.env.MONGO_URI}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log('Connected to DB'))
-.catch((err) => console.log(err));
