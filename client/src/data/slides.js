3c744489849a014d4b784bb8db83bad4adc20cec
Success-akinyemi <brownsamuel915@gmail.com>
2024-02-04 12:20:01 -0800

    FIre

--- client/src/data/slides.js
+++ client/src/data/slides.js
@@ -1,23 +1,43 @@
-import ImageOne from '../assets/Img4.png'
-import ImageTwo from '../assets/Img3.png'
+//import ImageOne from '../assets/Img4.png'
+//import ImageTwo from '../assets/Img3.png'
+
+
 import ImageThree from '../assets/Img2.png'
+import ImageFour from '../assets/women.png'
+import ImageFive from '../assets/bg1.png'
+import ImageSix from '../assets/Img1.png'
+import ImageSeven from '../assets/Img5.png'
+
+
 
 export const sliderData = [
     {
         _id: 1,
         text: 'We Thrive to reduce the poverty among widows in our society',
-        image: ImageOne
+        image: ImageThree
     },
 
     {
         _id: 2,
         text: 'We give to reduces the plight of the elderly',
-        image: ImageTwo
+        image: ImageFour
     },
 
     {
         _id: 3,
         text: 'We give to reduce poverty among the vulnerable',
-        image: ImageThree
+        image: ImageFive
+    },
+
+    {
+        _id: 4,
+        text: 'We give to reduce poverty among the vulnerable',
+        image: ImageSix
+    },
+
+    {
+        _id: 5,
+        text: 'We give to reduce poverty among the vulnerable',
+        image: ImageSeven
     }
 ]
